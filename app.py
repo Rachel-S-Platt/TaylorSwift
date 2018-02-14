1ca24f7ea9a9a4b513f47b032bd2836f5733a91f
Rachel Platt <rachel.s.platt@colorado.edu>
2018-02-13 23:23:57 -0700

    DOnt mind how ugly it is. U can upload a pic to a folder now

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
  
 app = Flask(__name__)
 str1='Wazzzzzupp'
+
+photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app, photos)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        return filename
+    return render_template('index.html')
  
 @app.route('/<string:page_name>/')
 def render_static(page_name):
